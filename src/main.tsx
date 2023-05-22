638c85e0accd408839b2616eb13a509f9a6d968d
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-05-22 15:59:59 -0300

    Creo el theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 import "./index.css"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { easyEatTheme } from "./theme/theme"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      <ThemeProvider theme={ easyEatTheme }>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
 )
